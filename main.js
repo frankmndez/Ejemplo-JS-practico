c86ad602631bbc45b8ab4fece95b49e8d288ae2f
frankmndez <frankmndez@outlook.com>
2022-08-22 18:19:20 -0500

    Modification for mobile Menu

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuMobileButton = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
+// Email submenu for desktop
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+// Menu for mobile
+
+menuMobileButton.addEventListener('click', toogleMenuMobile);
+
+function toogleMenuMobile(){
+    menuMobile.classList.toggle('inactive');
 }
